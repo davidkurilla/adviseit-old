3af03956b4414c520f683753a24155968b0ac080
David Kurilla <Kurilla.David@student.greenriver.edu>
2024-01-11 22:33:28 -0800

    DEV: Basic routing, UI, and GraphIT integration established

--- index.php
+++ index.php
@@ -10,12 +10,49 @@ require_once ('vendor/autoload.php');
 // Create instance of Base class
 $f3 = Base::instance();
 
-// Define default route
-$f3->route('GET /', function()
+// FUNCTION: getSortedClasses
+function getSortedClasses(): string
 {
-    $view = new Template();
-    echo $view->render('app/views/home.html');
+    $url= 'localhost:8080/courses/sorted';
+
+    //  Initiate curl
+    $ch = curl_init();
+    // Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Set the url
+    curl_setopt($ch, CURLOPT_URL,$url);
+    // Execute
+    $result = curl_exec($ch);
+    // Closing
+    curl_close($ch);
+
+    // Will dump a beauty json :3
+    // var_dump(json_decode($result, true));
+
+    return $result;
+
+}
+
+$f3->route('GET /', function() {
+   $view = new Template();
+   echo $view->render('app/views/home.html');
 });
 
+// Define AllCourses route
+$f3->route('GET /courses', function($f3)
+{
+    $raw = getSortedClasses();
+    $decoded = json_decode($raw, true);
+    //$data = print_r($decoded, true);
+    $f3->set('courses', $decoded);
+    $view = new Template();
+    echo $view->render('app/views/courses.html');
+});
+
+// Define preferences route
+$f3->route('GET /preferences', function() {
+    $view = new Template();
+    echo $view->render('app/views/preferences.html');
+});
 // Run Fat Free Framework
 $f3->run();
