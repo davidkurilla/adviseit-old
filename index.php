87bc8453d1d8532027913eafc72fe7d8dd8ec3b1
David Kurilla <kurilla.david@student.greenriver.edu>
2024-01-11 20:16:25 -0500

    Updated File: index.php | Configured file for F3

--- index.php
+++ index.php
@@ -3,3 +3,19 @@
 // Enable Error Reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+// Require autoload.php
+require_once ('vendor/autoload.php');
+
+// Create instance of Base class
+$f3 = Base::instance();
+
+// Define default route
+$f3->route('GET /', function()
+{
+    $view = new Template();
+    echo $view->render('app/views/home.html');
+});
+
+// Run Fat Free Framework
+$f3->run();
